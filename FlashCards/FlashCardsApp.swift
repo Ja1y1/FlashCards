0fe2c0142efbf9d3d0704ebedd4a4b3775852493
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-10-08 14:42:31 +0300

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by JAY on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
